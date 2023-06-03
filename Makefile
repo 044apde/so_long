9f1f9feb98e84d7e8c757d3b6d6574d75fb1975b
Shin Kim <044apde@gmail.com>
2023-06-03 17:00:35 +0900

    dot 5,5

--- Makefile
+++ Makefile
@@ -1,5 +1,39 @@
-all : 
-	cc -lmlx -framework OpenGl -framework AppKit -lmlx *.c
+# 메이크 파일 (Makefile)
 
+# 컴파일러 설정
+CC = gcc
+
+# 컴파일 및 링크 옵션
+CFLAGS = -Wall -Wextra -Werror
+LDFLAGS = -Lmlx -lmlx -framework OpenGL -framework AppKit
+
+# 라이브러리 및 실행 파일 이름
+LIBRARY = mlx/libmlx.a
+EXECUTABLE = so_long
+
+# 기본 타겟 (all) 설정
+all: $(EXECUTABLE)
+
+# 실행 파일 생성 타겟
+$(EXECUTABLE): $(LIBRARY) so_long.c
+	$(CC) $(CFLAGS) -o $@ so_long.c $(LIBRARY) $(LDFLAGS)
+
+# 라이브러리 파일 생성 타겟
+$(LIBRARY):
+	$(MAKE) -C mlx
+
+# 메이크 파일을 실행하여 생성된 파일 및 라이브러리 삭제
 clean:
-	rm -rf a.out
+	$(MAKE) -C mlx clean
+	rm -f $(EXECUTABLE)
+
+# 실행 파일 및 라이브러리 모두 삭제
+fclean: clean
+	$(MAKE) -C mlx clean
+	rm -f $(LIBRARY)
+	rm -f a.out
+
+# 전체 빌드 과정 삭제
+re: fclean all
+
+.PHONY: all clean fclean re
--- so_long.c
+++ so_long.c
@@ -5,23 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: shikim <shikim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/03 10:13:45 by shikim            #+#    #+#             */
-/*   Updated: 2023/06/03 11:29:43 by shikim           ###   ########.fr       */
+/*   Created: 2023/06/03 16:59:04 by shikim            #+#    #+#             */
+/*   Updated: 2023/06/03 17:00:03 by shikim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "mlx/mlx.h"
 
-typedef struct s_data
-{
+typedef struct	s_data {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}	t_data;
+}				t_data;
 
-void my_mlx_pixel_put(t_data  *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
@@ -38,12 +37,9 @@ int	main(void)
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	for(int i = 0; i < 10000 ; i++) {
-		my_mlx_pixel_put(&img, i, i, 0x00FF0000);// 붉은색 선을 대각선으로 그린다.
-		my_mlx_pixel_put(&img, 5, i, 0x00FF0000);// 붉은색 선을 세로으로 그린다.
-		my_mlx_pixel_put(&img, i, 5, 0x00FF0000);// 붉은색 선을 가로으로 그린다.
-	}
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);//이미지를 윈도우에 올린다.
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
